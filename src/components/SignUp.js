76981e689ade7193ede65dae1d0c940f725ee850
bwennuh <brennabruening@gmail.com>
2021-07-01 18:16:02 -0400

    Update login logic + add associated logged in user info to home page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -12,7 +12,30 @@ export default class SignUp extends Component {
     email: "",
     address: "",
     bio: "",
-    avatar: ""
+    avatar: "",
+    display: "sign-up"
+  }
+
+  changeToUserInfo = () => {
+    this.setState({
+      display: "user-info"
+    })
+  }
+
+  changeToSignUpStart = () => {
+    this.setState({
+      display: "sign-up"
+    })
+  }
+
+  submitUsernamePasswordHandler = (event) => {
+    event.preventDefault()
+    console.log("Testing submit username + password handler")
+  }
+
+  submitUserInfoHandler = (event) => {
+    event.preventDefault()
+    console.log("Testing submit user info handler")
   }
 
   render(){
@@ -21,6 +44,49 @@ export default class SignUp extends Component {
         <Navbar />
         <div>
           <h1>SIGN-UP PAGE</h1>
+
+          { this.state.display === "sign-up" ?
+          <div className="username-password-form">
+            <form onSubmit={(event) => this.submitUsernamePasswordHandler(event)}>
+              <label>Choose a username:</label><br></br>
+              <input placeholder="username"></input><br></br>
+
+              <label>Choose a password:</label><br></br>
+              <input placeholder="password"></input><br></br>
+
+              <label>Re-enter password:</label><br></br>
+              <input placeholder="re-enter password"></input><br></br>
+
+              <button type="submit">Submit</button>
+
+            </form>
+
+            <button onClick={() => this.changeToUserInfo()}>Continue</button>
+          </div>
+          : null }
+
+          { this.state.display === "user-info" ?
+          <div className="user-info-form">
+            <form onSubmit={(event) => this.submitUserInfoHandler(event)}>
+              <label>First name:</label><br></br>
+              <input placeholder="first name"></input><br></br>
+
+              <label>Last name:</label><br></br>
+              <input placeholder="last name"></input><br></br>
+
+              <label>Email:</label><br></br>
+              <input placeholder="example@example.com"></input><br></br>
+
+              <button type="submit">Submit</button>
+
+            </form>
+
+            <button type="submit" onClick={() => this.props.changeToLogin()}>Continue to log in page:</button>
+
+          </div>
+          : null }
+
+
         </div>
       </div>
     )
