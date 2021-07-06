d1f06446e15786ba639dc001cbf180aed77b0cd3
bwennuh <brennabruening@gmail.com>
2021-07-06 12:59:20 -0400

    Add logic + fetch request for creating new boards

--- src/components/BoardCardContainer.js
+++ src/components/BoardCardContainer.js
@@ -7,7 +7,11 @@ const boardsURL = baseURL + 'boards'
 export default class BoardCardContainer extends Component {
 
   state = {
-    boards: []
+    boards: [],
+    name: "",
+    description: "",
+    userID: this.props.user.id,
+    showCreateBoardForm: false
   }
 
   componentDidMount = () => {
@@ -25,12 +29,72 @@ export default class BoardCardContainer extends Component {
     })
   }
 
+  toggleCreateNewBoardForm = () => {
+    this.setState({
+      showCreateBoardForm: !this.state.showCreateBoardForm
+    })
+  }
+
+  getNewBoardName = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  getNewBoardDescription = (event) => {
+    this.setState({
+      description: event.target.value
+    })
+  }
+
+  createNewUserBoard = (event) => {
+    event.preventDefault()
+
+    const newBoard = {
+      name: this.state.name, 
+      description: this.state.description,
+      user_id: this.state.userID
+    }
+
+    const reqObj = {}
+
+    reqObj.headers = {"Content-Type": "application/json"}
+    reqObj.method = "POST"
+    reqObj.body = JSON.stringify(newBoard)
+
+    fetch(boardsURL, reqObj)
+    .then(resp => resp.json())
+    .then(newBoard => this.setState({ 
+      boards: [...this.state.boards, newBoard],
+      name: "",
+      description: "",
+      userID: this.props.user.id,
+      showCreateBoardForm: false
+     }))
+  }
+
   render(){
 
     return(
       <div className="board-card-conatiner">
         <div className="board-cards">
           <h1>BOARD CARD CONTAINER</h1>
+          <button onClick={() => this.toggleCreateNewBoardForm()}>Add new board</button><br></br>
+          { this.state.showCreateBoardForm ? 
+            <div className="create-board-form">
+              <form onSubmit={(event) => this.createNewUserBoard(event)}>
+                <label>Board name:</label><br></br>
+                <input type="text" onChange={(event) => this.getNewBoardName(event)}  placeholder="Board Name" required></input><br></br>
+
+                <label>Board description:</label><br></br>
+                <input type="text" onChange={(event) => this.getNewBoardDescription(event)}  placeholder="Board Description"></input><br></br>
+
+                <button id="create-board-button" type="submit">Create board</button>
+              </form>
+            </div>
+           : null }
+
+
           {this.state.boards.map(board => 
           <BoardCard 
           key={board.id} 
